4a7fb69fdfbf00067e96f89dc6a063a2908494d8
Asa Strong <asastrong19@gmail.com>
2018-06-13 21:39:33 -0700

    describe bands function

--- rasterio/rasterio_and_planet.py
+++ rasterio/rasterio_and_planet.py
@@ -1,5 +1,6 @@
-import numpy as np
 import rasterio
+import numpy as np
+from pprint import pprint
 
 def describe_raster(sat_data):
     # Get the bounding box of this GeoTIFF
@@ -9,10 +10,27 @@ def describe_raster(sat_data):
     w_meters = (sat_data.bounds.right - sat_data.bounds.left) / sat_data.width
     h_meters = (sat_data.bounds.top - sat_data.bounds.bottom) / sat_data.height
 
-    print("Bounding Box: {} \nPixel Width: {} \nPixel Height: {} ".format(bounds, w_meters, h_meters))
+    #get coord system
+    cs = sat_data.crs
+
+    print("Bounding Box: {} \nPixel Width: {} \nPixel Height: {} \nCoordinate System: {}".format(bounds, w_meters, h_meters, cs))
+
+def describe_bands(sat_data):
+
+    array = sat_data.read()
+    stats = []
+
+    for band in array:
+        stats.append({
+        'min': band.min(),
+        'mean': band.mean(),
+        'median': np.median(band),
+        'max': band.max()})
+
+    pprint(stats)
 
 if __name__ == '__main__':
 
     sat_data = rasterio.open(r'data\20180326_181856_103c_3B_AnalyticMS.tif')
 
-    describe_raster(sat_data)
+    describe_bands(sat_data)
